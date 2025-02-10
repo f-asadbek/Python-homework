747be56ef5ae1aab67e58252825086bf1b9ab28b
f-asadbek <asadbekfayzullaev777@gmail.com>
2025-02-10 19:16:18 +0500

    Delete lesson-2/string-7.py

--- lesson-2/string-7.py
+++
@@ -1,7 +0,0 @@
-a = input("Enter a sentence: ")
-b = input("Enter a word you want to replace on your sentence: ")
-c = input("Enter a word you want to replace with: ")
-if b in a:
-    print("Output:", a.replace(b, c))
-else:
-    print("Entered word is not in a sentence")
