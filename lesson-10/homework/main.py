3ce225849da74d93f3c06ec6006b772744df127f
f-asadbek <asadbekfayzullaev777@gmail.com>
2025-03-07 18:39:16 +0500

    Delete lesson-10/homework directory

--- lesson-10/homework/main.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-api_key = "5fe23939ff48d0e9fa6ff98d6d95d8ee"
-url = f"https://api.themoviedb.org/3/discover/movie?api_key={api_key}"
-response = requests.get(url)
-print(response.json())
